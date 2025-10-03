e28ec182c13a8201f2aa235fa6c99159a8b31b1d
kawofong <14829553+kawofong@users.noreply.github.com>
2025-10-03 11:40:29 -0400

    feat: add springboot sample

---
+++ java-springboot/dataConverter/api/src/main/java/io/temporal/app/api/controllers/UsersController.java
@@ -0,0 +1,89 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 temporal.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.temporal.app.api.controllers;
+
+import io.temporal.api.enums.v1.WorkflowIdReusePolicy;
+import io.temporal.app.api.messages.UserPost;
+import io.temporal.app.domain.messages.workflows.StartMyWorkflowRequest;
+import io.temporal.app.domain.workflows.MyWorkflow;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowExecutionAlreadyStarted;
+import io.temporal.client.WorkflowNotFoundException;
+import io.temporal.client.WorkflowOptions;
+import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UsersController {
+
+  Logger logger = LoggerFactory.getLogger(UsersController.class);
+  @Autowired WorkflowClient temporalClient;
+
+  @Value("${spring.curriculum.task-queue}")
+  String taskQueue;
+
+  @PostMapping(
+      value = "/{id}",
+      consumes = {MediaType.APPLICATION_JSON_VALUE},
+      produces = {MediaType.APPLICATION_JSON_VALUE})
+  ResponseEntity<String> usersPost(@PathVariable String id, @RequestBody UserPost params) {
+    // if using Temporal...
+    return startWorkflow(id, params);
+  }
+
+  private ResponseEntity<String> startWorkflow(String id, UserPost params) {
+    final WorkflowOptions options =
+        WorkflowOptions.newBuilder()
+            .setTaskQueue(taskQueue)
+            .setWorkflowId(id)
+            .setRetryOptions(null)
+            .setWorkflowIdReusePolicy(
+                WorkflowIdReusePolicy.WORKFLOW_ID_REUSE_POLICY_REJECT_DUPLICATE)
+            .build();
+    var workflowStub = temporalClient.newWorkflowStub(MyWorkflow.class, options);
+    var wfArgs = new StartMyWorkflowRequest(params.id(), params.value());
+    // Start the workflow execution.
+    try {
+      var run = WorkflowClient.start(workflowStub::execute, wfArgs);
+      var headers = new HttpHeaders();
+      headers.setLocation(URI.create(String.format("/api/v1/users/%s", id)));
+      return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    } catch (WorkflowExecutionAlreadyStarted was) {
+      logger.info("Workflow execution already started: {}", id);
+      return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
